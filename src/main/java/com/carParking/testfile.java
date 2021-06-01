0738a03629e5ccc1a8ed2fb871454d01d296685e
parth sarthi <parthsarthi650@gmail.com>
2021-06-01 10:29:05 +0530

    First Commit

---
+++ src/main/java/com/carParking/testfile.java
@@ -0,0 +1,9 @@
+package com.carParking;
+
+public class testfile {
+	
+	public static void main(String[] args) {
+		System.out.println("test file ");
+	}
+
+}
---
+++ src/test/java/com/carParking/CarParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carParking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
